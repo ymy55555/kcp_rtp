6169fc5b3d979e994359016b2dcbbc114e7b6598
yang <47072707+ymy55555@users.noreply.github.com>
2019-06-01 14:46:31 +0800

    Create kcp_client.h

---
+++ kcp_client.h
@@ -0,0 +1,22 @@
+#ifndef _KCP_CLIENT_H_
+#define _KCP_CLIENT_H_
+#include "common.h"
+
+//#define CLIENT_BIND_SERVER
+
+typedef struct _udp_client_data_
+{
+	int sClientFd;
+	int sSysRunState;
+	int sWndSize;
+	int sUpdateTime;
+	struct sockaddr_in sClientAddr;
+}UDP_CLIENT_DATA;
+
+extern UDP_CLIENT_DATA g_client_data;
+
+
+
+
+
+#endif
