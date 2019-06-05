4e438b681a3a7f1a067ecb44282ab8f2bff548cb
yang <47072707+ymy55555@users.noreply.github.com>
2019-06-05 21:47:17 +0800

    Update kcp_server.h

--- kcp_server.h
+++ kcp_server.h
@@ -2,13 +2,15 @@
 #define __KCP_SERVER_H__
 #include "common.h"
 
+//反应当前实时数据
 typedef struct _udp_server_data_
 {
 	int sServerFd;
 	int sSysRunState;
 	int sWndSize;
 	int sUpdateTime;
-	struct sockaddr_in sServerAddr;
+	struct sockaddr_in stServerAddr;
+	struct sockaddr_in stTransAddr;
 }UDP_SERVER_DATA;
 
 extern UDP_SERVER_DATA g_server_data;
